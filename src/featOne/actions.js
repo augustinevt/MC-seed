26439856cf806426fb991b7d86986317855184d9
Augustine  von Trapp <augustinevontrapp@Augustines-MacBook-Air.local>
2017-03-08 21:57:59 -0800

    add test data

--- src/featOne/actions.js
+++ src/featOne/actions.js
@@ -1,8 +1,9 @@
-// import mockData from '../../mockData.json';
+import mockData from '../../MOCK_DATA.json';
 import { LOAD_REPORT } from './actionTypes';
 
 export const loadReport = () => (dispatch) => {
   setTimeout(function () {
+    console.log(mockData)
     dispatch({ type: LOAD_REPORT, payload: { food: 'yum'} });
-  }, 5000);
+  }, 500);
 }
